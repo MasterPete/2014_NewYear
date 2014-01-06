475771ad63525e6a608f3c4f3613ca4916a14274
Pete Chemsripong <pchemsri@thoughtworks.com>
2014-01-06 19:25:09 +1100

    Fix top level warning

--- app.rb
+++ app.rb
@@ -7,23 +7,14 @@ require 'CSV'
 set :haml, format: :html5
 set :bind, '0.0.0.0'
 
-@@wishes = []
-
-CSV.foreach("#{settings.root}/assets/source.csv") do |row|
-	@@wishes << { fullname: row[0], url: row[1], name: row[2], word: row[3]}
-end
-
-get '/' do
-  @wishes = @@wishes
-  haml :index
-end
 
 get '/:name' do
-  @wishes = @@wishes
-  haml :index
+    @wishes = [{name: "there"}]
+    CSV.foreach("#{settings.root}/assets/source.csv") do |row|
+        @wishes << { fullname: row[0], url: row[1], name: row[2], word: row[3]}
+    end
+    haml :index
 end
-
-
 
 get '/stylesheets/*.css' do
   content_type 'text/css', charset: 'utf-8'
